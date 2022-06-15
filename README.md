b0971564896b5856c1106b4b7c541f3b17bb3e85
ChristianMar <christianlecce@gmail.com>
2022-06-15 11:54:22 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# react-ts-base-app
+
+typescript base react app
+
+## external package
+
+- webpack 5
+- axios
+- final form
+- polyglot
+- redux 4
+- redux dev tools
+- material 5
---
+++ source/app/webapp/src/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { HashRouter } from 'react-router-dom';
+
+
+ReactDOM.render(
+    <HashRouter>
+      CIAO
+    </HashRouter>,
+  document.getElementById('root')
+);
