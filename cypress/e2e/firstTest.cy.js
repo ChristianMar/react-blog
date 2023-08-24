d09720f6603a0e41ca4423a883540940726dac14
ChristianMar <christianlecce@gmail.com>
2023-08-24 11:32:50 +0200

    fix: cypress configuration

---
+++ cypress/e2e/firstTest.cy.js
@@ -0,0 +1,9 @@
+/*eslint-disable*/
+
+describe('first-test', () => {
+  beforeEach(() => {
+    cy.visit('https://localhost:9000/#/');
+  });
+
+  it('displays two todo items by default', () => {});
+});
